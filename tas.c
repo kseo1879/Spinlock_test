bd1f650c723c862d2228875ec0cbdc65334740f0
Calvin (Kang Min) Seo <kseo6751@uni.sydney.edu.au>
2020-10-24 20:36:35 +1100

    Commenting Done

--- tas.c
+++ tas.c
@@ -14,27 +14,42 @@ struct tas {
     int *lock;
 };
 
+/**This structure is for TEST ONLY.
+ * When you test it will store the data and tas lock and 
+ * pass it to different threads.
+ */
 struct data {
     struct tas *tas;
     long *counter;
 };
 
-//This is initializing the state to the unlock state
+/**
+ * @pram taslock, initialises the handle for the taslock
+ */
 void tas_init(struct tas *t) {
     t->lock = malloc(sizeof(int));
     *(t->lock) = 0;
 }
 
+/**
+* Locks, operation should be successful if the lock is valid.
+* If the lock is invalid, the lock operation returns a non-zero integer
+* tas lock that is currently in a locked state will keep threads waiting. * @param taslock
+* @return success
+*/
 int tas_lock(struct tas *t) {
     int expected = UNLOCKED;
+    //Testing if lock is initialized.
     if(NULL == t) {
         perror("tas is not initialized\n");
         return -1;
     } 
+    //This will loop until the lock resourse is unlocked.
     while(!__atomic_compare_exchange_n(t->lock, &expected,
         LOCKED, 1, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
             expected = UNLOCKED;  
     }
+    //THis is to make sure that it leaved the funtion with the correct lock value.
     if(1 != *(t->lock)) {
         perror("Spin lock dident execute correctly\n");
         return -1;
@@ -42,8 +57,14 @@ int tas_lock(struct tas *t) {
     return 0;
 }
 
+
+/**
+* Unlocks, operation should be successful is the lock is valid.
+* If the lock is invalid, unlock operation returns a non-zero integer. * @param taslock
+* @return success
+*/
 int tas_unlock(struct tas *t) {
-    //Testing if the structure t is valid
+    //Testing if lock is initialized.
     if(NULL == t) {
         perror("tas is not initialized\n");
         return -1;
@@ -52,7 +73,10 @@ int tas_unlock(struct tas *t) {
     return 0;
 }
 
-//This will free the memory that was created 
+/**
+ * Destroys the tas lock, puts it in an invalid state
+ * @param taslock
+*/
 int tas_destory(struct tas *t) {
     free(t->lock);
     free(t);
@@ -62,14 +86,16 @@ int tas_destory(struct tas *t) {
 //Testing Function
 void* worker(void *args) {
     struct data *data = (struct data *)args;
-    // tas_lock(data->tas);
+    // if you change line 93 and 95 with 91 and 97 it still works.
+    // The intention here was to give more stree to the lock and unlock function.
+    // //tas_lock(data->tas);
     for(int i = 0; i < TEST_ITERATION; i++) {
         tas_lock(data->tas);
         *(data->counter) = *(data->counter) + 1;
         tas_unlock(data->tas);
     }
-    // tas_unlock(data->tas);
-
+    // //tas_unlock(data->tas);
+    // Without the lock specified it will created issues with unintended sharing.
 }
 
 //Testing Function
@@ -80,6 +106,7 @@ int main() {
     struct data data = {.tas = tas, .counter = &counter};
     pthread_t threads[N_THREADS];
 
+    //Passing the data value the the threads to change the value. 
     for(int i = 0; i < N_THREADS; i ++) {
         pthread_create(threads+i, NULL, worker, &data);
     }
